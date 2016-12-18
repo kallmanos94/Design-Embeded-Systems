56e865f89c8871d59b55b4d184dacc2e476a6e6c
kallmanos94 <kallmanos94@users.noreply.github.com>
2016-12-18 23:43:51 +0200

    Update proj1.c

--- proj1.c
+++ proj1.c
@@ -1,3 +1,68 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define IW 352
+#define IH 288
+#define EIW 356
+#define EIH 292
+#define N 5
+#define S 0.88
+#define K 3
+#define filename "ak.yuv"
+#define finalfilename "akout.yuv"
+
+int i,j;
+int image[IH][IW];
+double is[IH][IW];
+double g[N][N];
+int Rep[EIH][EIW];
+
+void read(){
+  FILE *frame_c;
+  if((frame_c=fopen(filename,"rb"))==NULL)
+  {
+    printf("current frame doesn't exist\n");
+    exit(-1);
+  }
+
+  for(i=0;i<IH;i++)
+  {
+    for(j=0;j<IW;j++)
+    {
+      image[i][j]=fgetc(frame_c);
+    }
+  }
+  fclose(frame_c);
+}
+
+void write(){
+  FILE *frame_y;
+  frame_y=fopen(finalfilename,"wb");
+
+  for(i=0;i<IH;i++)
+  {
+    for(j=0;j<IW;j++)
+    {
+      fputc(image[i][j],frame_y);
+    }
+  }
+  fclose(frame_y);
+}
+
+void calfin(){
+    for(i=0;i<IH;i++){
+        for(j=0;j<IW;j++){
+            double temp = image[i][j] + K*( image[i][j] - is[i][j] );
+            if(temp>255) image[i][j] = 255;
+            else if(temp<0) image[i][j] = 0;
+            else image[i][j] = temp;
+        }
+    }
+}
+
+void gauss(){
     double c = exp(N*N/(S*S));
 
     int a = -N/2;
